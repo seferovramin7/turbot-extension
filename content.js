09266ce26c8ce9471c55d021d2990a21dd00dee8
safarovrag <ramin.safarov@kapitalbank.az>
2023-07-25 23:42:46 +0400

    works well

--- content.js
+++ content.js
@@ -1,15 +1,6 @@
 var userId;
-
-
 chrome.storage.sync.get("userID", function (result) {
-    // The result is an object with the key-value pairs from the storage.
-    // In this case, result.userId will give you the value of the "userId" key.
     userId = result.userID;
-
-    // Now you can use the userId string as needed.
-    console.log("User ID: " + userId);
-
-    // Any code that requires the userId should be placed here or called from here.
 });
 
 
@@ -30,7 +21,6 @@ function sendProductPriceToServer() {
     }
 }
 
-// Create a button element
 const button = document.createElement('button');
 button.textContent = 'Avtomobili izləməyə al';
 button.addEventListener('click', sendProductPriceToServer);
@@ -57,55 +47,19 @@ button.addEventListener('mouseover', function () {
 button.addEventListener('mouseout', function () {
     button.style.backgroundColor = 'red';
 });
-
-// Find a suitable location to append the button
 const container = document.querySelector('.product-phones__btn');
-
-// Check if the container element exists before appending the button
 if (container) {
     container.appendChild(button);
 } else {
     console.error('Container element not found.');
 }
 
-
-// Function to update the dropdown options with the retrieved car list
 function updateDropdown(carList) {
     const dropdown = document.getElementById("carDropdown");
-
-    // Clear existing options
     dropdown.innerHTML = "";
-
-    // Add new options based on the retrieved car list
     carList.forEach(car => {
         const option = document.createElement("option");
         option.text = car;
         dropdown.add(option);
     });
 }
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     fetch('http://localhost:8080/list/search?chatId=' + userId)
-//         .then(response => response.json())
-//         .then(data => {
-//             const carLinksDropdown = document.getElementById('searchLinksDropdown');
-//             data.forEach(link => {
-//                 const option = document.createElement('option');
-//                 option.text = link;
-//                 carLinksDropdown.appendChild(option);
-//             });
-//             carLinksDropdown.addEventListener('change', function () {
-//                 const selectedOption = carLinksDropdown.options[carLinksDropdown.selectedIndex];
-//                 const url = selectedOption.value;
-//                 if (url) {
-//                     // Open the URL in a new tab/window
-//                     window.open(url, '_blank');
-//                 }
-//             });
-//         })
-//         .catch(error => {
-//             console.error('Error fetching car links:', error);
-//         });
-// });
-
